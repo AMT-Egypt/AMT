257a54a4555d906779b3ee8fa131ddad3afa0b3a
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-08-05 04:12:49 +0300

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import "./Home.css"
+import "./Home.css";
 import Button from "../Button/Button";
+
 export default function App() {
     return (
         <>
             <Navbar />
             <section className="home_sec">
                 <div className="title">
-                    <h1>transform our Brand Presence With Powerful Visual Stories <span>Experience</span></h1>
-                    <p>we Empower brands by creating compelling visual identities that tell your unique story making your brand unforgettable</p>
-                    <Button h={10} w={10} title={"contact US"} event={
-                        window.open("#") // Social Media Link
-                    } />
+                    <h1>Transform Our Brand Presence With Powerful Visual Stories <span>Experience</span></h1>
+                    <p>We empower brands by creating compelling visual identities that tell your unique story making your brand unforgettable.</p>
+                    <Button h={5} w={10} title={"Contact Us"} event={() => window.open("#")} />
                 </div>
                 <div className="IconsParent">
-                    <div><i class="fa-brands fa-github"></i></div>  {/*Github*/}
-                    <div><i class="fa-brands fa-whatsapp"></i></div> {/*WhatsApp*/}
-                    <div><i class="fa-brands fa-facebook"></i></div> {/*Facebook*/}
+                    <div><i className="fa-brands fa-github"></i></div> {/* GitHub */}
+                    <div><i className="fa-brands fa-whatsapp"></i></div> {/* WhatsApp */}
+                    <div><i className="fa-brands fa-facebook"></i></div> {/* Facebook */}
                 </div>
             </section>
         </>
