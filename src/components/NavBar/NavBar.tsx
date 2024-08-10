d7bf2dc161a37b9fe005948a8f934fcaf4a0a283
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-08-10 17:12:34 +0300

    Add files via upload

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,35 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import logo from "../../assets/Logo.svg";
 import { CloseIcon, ContactUsIcon, TwoLinesIcon } from "../../icons/icons";
 import "../../style/Navbar.css";
-export default function NavBar() {
+
+// Define the type for social media links
+interface SocialLink {
+  name: string;
+  url: string;
+}
+
+// Define the type for NavItem props
+interface NavItemProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+// Define the type for SocialMediaMenu props
+interface SocialMediaMenuProps {
+  links: SocialLink[];
+}
+
+const socialLinks: SocialLink[] = [
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/profile.php?id=61559455807819&mibextid=ZbWKwL",
+  },
+  { name: "WhatsApp", url: "https://wa.me/+201019472864" },
+  { name: "Github", url: "https://github.com/AMT-Egypt" },
+];
+
+const NavBar: React.FC = () => {
   const [menuVisible, setMenuVisible] = useState(false);
 
-  const handleMenuToggle = () => {
-    setMenuVisible(!menuVisible);
-  };
+  const handleMenuToggle = () => setMenuVisible((prev) => !prev);
+  const handleCloseMenu = () => setMenuVisible(false);
+  const reloadWindow = () => window.location.reload();
 
-  const handleCloseMenu = () => {
-    setMenuVisible(false);
-  };
-
-  let handleContact = () => {
-    window.open("https://wa.me/+201019472864");
-  };
-  let goToGithub = () => {
-    window.open("https://github.com/AMT-Egypt");
-  };
-  let goToFacebook = () => {
-    window.open(
-      "https://www.facebook.com/profile.php?id=61559455807819&mibextid=ZbWKwL"
-    );
-  };
-  let goToBehince = () => {
-    window.open("#");
-  }; // AMT Desginer Portfolio Link
-  let reloadWindow = () => {
-    window.location.reload();
-  };
   return (
     <header className="Navbar" id="navbar">
       <img onClick={reloadWindow} className="logo" src={logo} alt="logo" />
@@ -40,36 +46,37 @@ export default function NavBar() {
         <button className="close-menu-button" onClick={handleCloseMenu}>
           <CloseIcon />
         </button>
+        <NavItem href="#">Home</NavItem>
+        <NavItem href="#work">Work</NavItem>
+        <NavItem href="#service">Services</NavItem>
+        <div className="bg_nav_menu"></div>
+        <div className="shareIcon">
+          <NavItem href="#contact">Contact us</NavItem>
+          <ContactUsIcon />
+        </div>
+
         <li>
-          <a href="#/">Home</a>
-        </li>
-        <li>
-          <a href="#/">About</a>
-        </li>
-        <li>
-          <a href="#work">Work</a>
-        </li>
-        <li>
-          <a href="#service">Services</a>
-        </li>
-        <div style={{ fontWeight: "400" }} className="bg_nav_menu"></div>
-        <div className="hrTag"></div>
-        <li>
-          <a className="contactUS" href="#contact">
-            <div>Contact us</div>
-            <div className="shareIcon">
-              {" "}
-              <ContactUsIcon />
-            </div>
-          </a>
-          <div className="socialMediaMenu">
-            <div onClick={goToFacebook}>facebook</div>
-            <div onClick={handleContact}>What's App</div>
-            <div onClick={goToGithub}>Github</div>
-            <div onClick={goToBehince}>Behince</div>
-          </div>
+          <SocialMediaMenu links={socialLinks} />
         </li>
       </ul>
     </header>
   );
-}
+};
+
+const NavItem: React.FC<NavItemProps> = ({ href, children }) => (
+  <li>
+    <a href={href}>{children}</a>
+  </li>
+);
+
+const SocialMediaMenu: React.FC<SocialMediaMenuProps> = ({ links }) => (
+  <div className="socialMediaMenu">
+    {links.map(({ name, url }) => (
+      <div key={name} onClick={() => window.open(url)}>
+        {name}
+      </div>
+    ))}
+  </div>
+);
+
+export default NavBar;
