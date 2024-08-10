cf0b5d8ea36acf64ecaa86b2e9e579513ea7b69f
Hamdy Mohamed Askander <121273938+hamdymohamedak@users.noreply.github.com>
2024-08-10 12:42:11 +0300

    Add files via upload

--- src/components/OurWork/WorkCard.tsx
+++ src/components/OurWork/WorkCard.tsx
@@ -3,14 +3,14 @@ interface WorkCardTsxProps {
   title: string;
   url: string;
 }
-export default function WorkCard({image,title,url}: WorkCardTsxProps) {
-  const goToProject = ()=>{
+export default function WorkCard({ image, title, url }: WorkCardTsxProps) {
+  const goToProject = () => {
     window.open(url, "_blank");
-  }
+  };
   return (
-    <div onClick={()=>goToProject()} className="back">
-      <img src={image} alt="work imag" />
-      <div className="absult">
+    <div onClick={() => goToProject()} className="back">
+      <img style={{ objectFit: "contain" }} src={image} alt="work imag" />
+      <div style={{ background: "transparent" }} className="absult">
         <h1>{title}</h1>
         <p>We&apos;re all about taking your business to the next level. </p>
       </div>
